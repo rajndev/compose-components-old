0bd9dbe39cede817e24fd0693734729e5644ba15
rajndev <rajnsoftwaredev@gmail.com>
2022-08-30 00:31:50 -0400

    Implement a working CustomAppBar with correct title layout

--- app/src/main/java/com/shivnasoft/composecomponents/MainActivity.kt
+++ app/src/main/java/com/shivnasoft/composecomponents/MainActivity.kt
@@ -5,17 +5,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.DrawerValue
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.rememberDrawerState
 import androidx.compose.material.rememberScaffoldState
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
+import androidx.compose.material3.AlertDialogDefaults.textContentColor
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
 import com.shivnasoft.compose_components.CustomAppBar
@@ -32,12 +40,51 @@ class MainActivity : ComponentActivity() {
             ComposeComponentsTheme {
                 navController = rememberAnimatedNavController()
                 val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+
+                Scaffold(
+                    topBar = {
+                        CustomAppBar(
+                            title = "Testing Bar",
+                            actions = {
+                                IconButton(onClick = { /* doSomething() */ }) {
+                                    Icon(
+                                        imageVector = Icons.Filled.Favorite,
+                                        contentDescription = "Localized description"
+                                    )
+                                }
+                            },
+                            navController = navController,
+                            scaffoldState = scaffoldState
+                        )
+                    },
+                    content = { innerPadding ->
+                        LazyColumn(
+                            contentPadding = innerPadding,
+                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            val list = (0..75).map { it.toString() }
+                            items(count = list.size) {
+                                Text(
+                                    text = list[it],
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(horizontal = 16.dp)
+                                )
+                            }
+                        }
+                    }
+                )
+
+               // SimpleSmallTopAppBar()
+
+
                 // A surface container using the 'background' color from the theme
                 /*Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {*/
-                Scaffold(
+              /*  Scaffold(
                     topBar = {
                         CustomAppBar(
                             "Test Bar",
@@ -51,9 +98,60 @@ class MainActivity : ComponentActivity() {
                 )
                 {
                     Text("It Works, It Works! :)")
-                }
+                }*/
                 //}
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SimpleSmallTopAppBar() {
+    Scaffold(
+        topBar = {
+            SmallTopAppBar(
+                title = {
+                    Text(
+                        "Simple TopAppBar",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Favorite,
+                            contentDescription = "Localized description"
+                        )
+                    }
+                }
+            )
+        },
+        content = { innerPadding ->
+            LazyColumn(
+                contentPadding = innerPadding,
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                val list = (0..75).map { it.toString() }
+                items(count = list.size) {
+                    Text(
+                        text = list[it],
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp)
+                    )
+                }
+            }
+        }
+    )
 }
--- compose-components/src/main/java/com/shivnasoft/compose_components/CustomAppBar.kt
+++ compose-components/src/main/java/com/shivnasoft/compose_components/CustomAppBar.kt
@@ -1,7 +1,5 @@
 package com.shivnasoft.compose_components
 
-import androidx.compose.foundation.clickable
-
 import androidx.compose.material.IconButton
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.icons.Icons
@@ -10,8 +8,8 @@ import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.navigation.NavController
 import kotlinx.coroutines.launch
 
@@ -19,20 +17,29 @@ import kotlinx.coroutines.launch
 @Composable
 fun CustomAppBar(
     title: String,
-    backGroundColor: Color = Color.White,
+    titleContentColor: Color = Color.Black,
+    containerBackGroundColor: Color = Color.White,
     actions: @Composable () -> Unit = { },
-    navController: NavController,
+    navController: NavController? = null,
     scaffoldState: ScaffoldState? = null,
 ) {
     val scope = rememberCoroutineScope()
 
     SmallTopAppBar(
-        title = { Text(text = title) },
+        title = {
+            Text(
+                title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
         colors = TopAppBarDefaults.smallTopAppBarColors(
-            containerColor = backGroundColor
+            containerColor = containerBackGroundColor,
+            titleContentColor = titleContentColor
         ),
-        navigationIcon = if (navController.previousBackStackEntry != null) {
-            {   IconButton(onClick = { navController.navigateUp() }) {
+        navigationIcon = if (navController?.previousBackStackEntry != null) {
+            {
+                IconButton(onClick = { navController.navigateUp() }) {
                     Icon(
                         imageVector = Icons.Filled.ArrowBack,
                         contentDescription = "Back"
@@ -40,21 +47,53 @@ fun CustomAppBar(
                 }
             }
         } else {
-        {
-            Icon(
-                Icons.Filled.Menu,
-                contentDescription = "Nav drawer icon",
-                modifier = Modifier.clickable(onClick = {
+            {
+                IconButton(onClick = {
                     scope.launch {
                         scaffoldState?.drawerState?.open()
                     }
-                })
-            )
-        }
-
+                }) {
+                    Icon(
+                        Icons.Filled.Menu,
+                        contentDescription = "Nav drawer icon",
+                    )
+                }
+            }
         },
         actions = {
             actions()
         }
     )
+
+    /* SmallTopAppBar(
+         title = { Text(text = title) },
+         colors = TopAppBarDefaults.smallTopAppBarColors(
+             containerColor = backGroundColor
+         ),
+         navigationIcon = if (navController.previousBackStackEntry != null) {
+             {   IconButton(onClick = { navController.navigateUp() }) {
+                     Icon(
+                         imageVector = Icons.Filled.ArrowBack,
+                         contentDescription = "Back"
+                     )
+                 }
+             }
+         } else {
+         {
+             Icon(
+                 Icons.Filled.Menu,
+                 contentDescription = "Nav drawer icon",
+                 modifier = Modifier.clickable(onClick = {
+                     scope.launch {
+                         scaffoldState?.drawerState?.open()
+                     }
+                 })
+             )
+         }
+
+         },
+         actions = {
+             actions()
+         }
+     )*/
 }
