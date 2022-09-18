4e7ad93ee4b49618bab667c6c65711829581f374
rajndev <rajnsoftwaredev@gmail.com>
2022-09-18 18:52:08 -0400

    Bugfix: aligned morevert dropdown menu to the morevert icon

--- compose-components/src/main/java/com/shivnasoft/compose_components/appbars/MorevertDropdownMenu.kt
+++ compose-components/src/main/java/com/shivnasoft/compose_components/appbars/MorevertDropdownMenu.kt
@@ -1,12 +1,11 @@
 package com.shivnasoft.compose_components.appbars
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
@@ -18,38 +17,45 @@ fun MorevertDropdownMenu(
     onMenuItemClick: (Any?, String) -> Unit
 ) {
     var showMenu by remember { mutableStateOf(false) }
-
-    IconButton(onClick = {
-        showMenu = !showMenu
-    }) {
-        Icon(
-            imageVector = Icons.Outlined.MoreVert,
-            contentDescription = "Overflow menu",
-        )
-    }
-    MaterialTheme(shapes = MaterialTheme.shapes.copy(medium = RoundedCornerShape(dropDownListRoundedCornerAmount))) {
-        DropdownMenu(
-            expanded = showMenu,
-            onDismissRequest = { showMenu = false }
+    Column {
+        IconButton(onClick = {
+            showMenu = !showMenu
+        }) {
+            Icon(
+                imageVector = Icons.Outlined.MoreVert,
+                contentDescription = "Overflow menu",
+            )
+        }
+        MaterialTheme(
+            shapes = MaterialTheme.shapes.copy(
+                medium = RoundedCornerShape(
+                    dropDownListRoundedCornerAmount
+                )
+            )
         ) {
-            when {
-                dropDownList != null -> {
-                    dropDownList.forEach { label ->
-                        DropdownMenuItem(onClick = {
-                            onMenuItemClick(null, label)
-                            showMenu = false
-                        }) {
-                            Text(text = label)
+            DropdownMenu(
+                expanded = showMenu,
+                onDismissRequest = { showMenu = false }
+            ) {
+                when {
+                    dropDownList != null -> {
+                        dropDownList.forEach { label ->
+                            DropdownMenuItem(onClick = {
+                                onMenuItemClick(null, label)
+                                showMenu = false
+                            }) {
+                                Text(text = label)
+                            }
                         }
                     }
-                }
-                dropDownListMap != null -> {
-                    dropDownListMap.forEach { (key, value) ->
-                        DropdownMenuItem(onClick = {
-                            onMenuItemClick(key, value)
-                            showMenu = false
-                        }) {
-                            Text(text = value)
+                    dropDownListMap != null -> {
+                        dropDownListMap.forEach { (key, value) ->
+                            DropdownMenuItem(onClick = {
+                                onMenuItemClick(key, value)
+                                showMenu = false
+                            }) {
+                                Text(text = value)
+                            }
                         }
                     }
                 }
